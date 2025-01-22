9be80294b0d10bb29ff25e2673274890d8b2307f
miura hiroto <sanpuhiroto469@gmail.com>
2025-01-22 10:45:41 +0900

    rewrite index.py

--- api/index.py
+++ api/index.py
@@ -7,7 +7,7 @@ PARENT_DIR = os.path.dirname(CURRENT_DIR)
 sys.path.append(PARENT_DIR)
 
 # 2) settings.py を指定
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mikan_project.settings")
 
 # 3) Djangoを起動
 import django
